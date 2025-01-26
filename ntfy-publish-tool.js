b0d77426563b74f674e8ce3ed938e189e6664b8e
hotine <hotine@163.com>
2025-01-26 11:02:09 +0000

    。

--- ntfy-publish-tool.js
+++ ntfy-publish-tool.js
@@ -1,12 +1,12 @@
 /**
  * 发送推送消息到ntfy服务
- * @param {string} topic - 消息的主题
- * @param {string} message - 要发送的消息内容
+ * @param {string} topic - 消息的主题（必填）
+ * @param {string} message - 要发送的消息内容（必填）
  * @param {string} title - 消息的大标题(默认不使用大标题)
- * @param {int} [priority=3] - 消息的优先级，可以是1-5的整数，分别是最小、小、默认、大、最大
- * @param {array} [tags] - 消息的标签,字符串数组。
- * @param {array} [attach] - 附件、图片URL。
- * @param {array} [click] - 消息被点击时跳转的url。
+ * @param {int} [priority=3] - 消息的优先级（默认是3），可以是1-5的整数，分别是最小、小、默认、大、最大
+ * @param {array} [tags] - 消息的标签,字符串数组（默认无）
+ * @param {array} [attach] - 附件、图片URL（默认无）
+ * @param {array} [click] - 消息被点击时跳转的url（默认无）
  * @param {string} [serverUrl='https://ntfy.sh'] - ntfy服务的URL,默认为官方服务器
  */
 async function sendNtfyMessage(topic, message, title = null,  priority = 3, tags = null, attach = null , click = null , serverUrl = 'https://ntfy.sh') {
